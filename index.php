24cd42c72908a8b69aa251f237aa2ac96a4dae75
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-10-13 22:40:19 +0800

    added reg form

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <title>BCD Test Site</title>
 </head>
 <body>
-  
+
+<div class="navcontainer">
     <nav class="nav">
         <div class="navlogo">
             <img src="./assets/logo.png" alt="Company X">
@@ -28,26 +30,86 @@
             </li>
         </ul>
     </nav>
-        
 
-    <div class="hero">
+    
+</div>
+
+<div class="hero">
         <div class="hero-image">
             <img src="./assets/header.png" alt="Run For Earth" class="header-image">
-            <p>A 3k run alongside the candidates of Miss Philippines Earth 2015</p>
-            <p>to aid the Calumpang River Rehabilitation</p>
+            <p class="desc">A 3k run alongside the candidates of Miss Philippines Earth 2015</p>
+            <p class="desc">to aid the Calumpang River Rehabilitation</p>
         </div>
         <div class="hero-text">
-            <p>APRIL 25, 2015 | Assembly time: 5:00AM | SM City Batangas Parking Grounds</p>
-            <p>Registration fee: Php500 inclusive of race kit with shirt</p>
+            <p class="event-details">APRIL 25, 2015 | Assembly time: 5:00AM | SM City Batangas Parking Grounds</p>
+            <p class="registration-fee">Registration fee: Php500 inclusive of race kit with shirt</p>
             <button class="register">Register Now!</button>
         </div>
     </div>
+        
+
+    
    
 
-    <!-- <main>
-        <section id="register">
-            
+    <main>
+        <section id="sign-up">
+            <div class="form-container">
+                <div class="register-head">
+                    <h1 class="section-title">SIGN UP</h1>
+                    <img src="./assets/section-divider.png" alt="Section divider">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel cumque magni perferendis architecto, atque quaerat cupiditate odit qui repellat nostrum illum consectetur asperiores debitis ducimus? Quam sequi magni laborum maiores!</p>
+                </div>
+    
+                <div>
+                    <form action="" method="POST">
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" placeholder="First Name">
+                            </div>
+                            <div class="col">
+                                <input type="text" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" placeholder="Complete Address">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" placeholder="Birthday">
+                            </div>
+                            <div class="col">
+                                <input type="number" placeholder="Contact Number">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="email" placeholder="Email Address">
+                            </div>
+                            <div class="col">
+                                <select>
+                                    <option value="" selected disabled>Shirt size</option>
+                                    <option>S</option>
+                                    <option>M</option>
+                                    <option>L</option>
+                                    <option>XL</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </section>
-    </main> -->
+    </main>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
