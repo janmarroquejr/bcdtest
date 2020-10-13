b49fe3e5ce66cb08aad13df554f70cae4a4d0180
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-10-13 20:30:18 +0800

    added header

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>BCD Test Site</title>
+</head>
+<body>
+  
+    <nav class="nav">
+        <div class="navlogo">
+            <img src="./assets/logo.png" alt="Company X">
+        </div>
+    
+        <ul class="navlinks">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Run Information</a>
+            </li>
+            <li>
+                <a href="">Registration</a>
+            </li>
+            <li>
+                <a href="">Contact Us</a>
+            </li>
+        </ul>
+    </nav>
+        
+
+    <div class="hero">
+        <div class="hero-image">
+            <img src="./assets/header.png" alt="Run For Earth" class="header-image">
+            <p>A 3k run alongside the candidates of Miss Philippines Earth 2015</p>
+            <p>to aid the Calumpang River Rehabilitation</p>
+        </div>
+        <div class="hero-text">
+            <p>APRIL 25, 2015 | Assembly time: 5:00AM | SM City Batangas Parking Grounds</p>
+            <p>Registration fee: Php500 inclusive of race kit with shirt</p>
+            <button class="register">Register Now!</button>
+        </div>
+    </div>
+   
+
+    <!-- <main>
+        <section id="register">
+            
+        </section>
+    </main> -->
+</body>
+</html>
